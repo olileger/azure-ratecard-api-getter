3ad4233340433bfa3dd97639d6f6391b07d3b687
olileger <olileger@microsoft.com>
2018-03-22 18:03:09 +0100

    Development of RateCard API call

--- test/getprice.js
+++ test/getprice.js
@@ -3,10 +3,21 @@ let func = require("../index").GetPriceAndMetadataInformation;
 
 describe("GetPriceAndMetadataInformation()", function()
 {
-    
-    it("Nominal case", function()
+    let config =
     {
-        func();
+        aad_tenant_id : process.env.aad_tenant_id,
+        application_id : process.env.application_id,
+        application_secret : process.env.application_secret,
+        subscription_id : process.env.subscription_id,
+        offer_id : "MS-AZR-0003P",
+        currency : "EUR",
+        locale : "en-us",
+        region : "FR"
+    };
+
+    it("Nominal case", function(done)
+    {
+        func(config, done);
         //expect()...
     });
 
